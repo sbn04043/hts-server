643f26dcde38254f71a56a5550fcdc5ecbb2a4c4
김준수 <sbn04043@gmail.com>
2021-06-15 21:04:34 +0900

    빠진 파일 추가

---
+++ .eslintrc.js
@@ -0,0 +1,24 @@
+module.exports = {
+  extends: [
+    "airbnb-typescript/base",
+    "plugin:@typescript-eslint/eslint-recommended",
+    "plugin:@typescript-eslint/recommended",
+    "plugin:@typescript-eslint/recommended-requiring-type-checking",
+    "prettier",
+  ],
+  parserOptions: {
+    project: "./tsconfig.json",
+    ecmaVersion: 2018,
+    sourceType: "module",
+  },
+  globals: {
+    Atomics: "readonly",
+    SharedArrayBuffer: "readonly",
+  },
+  rules: {
+    "max-len": ["error", { code: 150, ignoreRemplateLiterals: true }],
+    "@typescript-eslint/no-non-null-assertion": "off",
+    "@typescript-eslint/lines-between-class-members": "off",
+    "import/prefer-default-export": "off",
+  },
+};
