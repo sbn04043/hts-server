dbc5ff808882c3fb7f20e79c17841ff1fb85de5a
김준수 <sbn04043@gmail.com>
2021-06-17 21:53:19 +0900

    데이터 추가

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,24 +1,24 @@
 module.exports = {
   extends: [
-    "airbnb-typescript/base",
-    "plugin:@typescript-eslint/eslint-recommended",
-    "plugin:@typescript-eslint/recommended",
-    "plugin:@typescript-eslint/recommended-requiring-type-checking",
-    "prettier",
+    'airbnb-typescript/base',
+    'plugin:@typescript-eslint/eslint-recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:@typescript-eslint/recommended-requiring-type-checking',
+    'prettier',
   ],
   parserOptions: {
-    project: "./tsconfig.json",
+    project: './tsconfig.json',
     ecmaVersion: 2018,
-    sourceType: "module",
+    sourceType: 'module',
   },
   globals: {
-    Atomics: "readonly",
-    SharedArrayBuffer: "readonly",
+    Atomics: 'readonly',
+    SharedArrayBuffer: 'readonly',
   },
   rules: {
-    "max-len": ["error", { code: 150, ignoreRemplateLiterals: true }],
-    "@typescript-eslint/no-non-null-assertion": "off",
-    "@typescript-eslint/lines-between-class-members": "off",
-    "import/prefer-default-export": "off",
+    'max-len': ['error', { code: 150, ignoreRemplateLiterals: true }],
+    '@typescript-eslint/no-non-null-assertion': 'off',
+    '@typescript-eslint/lines-between-class-members': 'off',
+    'import/prefer-default-export': 'off',
   },
 };
